187ed128f754a1f6e1b8075dc0f3065fd6eabe72
Samuel Wai Okyere <90344401+WAIOKYERE@users.noreply.github.com>
2024-02-16 06:50:28 +0000

    Add files via upload

---
+++ resources/scripts/common-nav-script.js
@@ -0,0 +1,27 @@
+// JavaScript for Navbar Toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+// JavaScript for Slideshow
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    const slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
